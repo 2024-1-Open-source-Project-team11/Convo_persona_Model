4cc964d0d4b35ea3958d0d21047b7165cb043f8f
Taeyoung Kim <tyk226@naver.com>
2024-05-12 12:26:46 +0900

    add oversampling(SMOTE)

--- Convopersona.py
+++ Convopersona.py
@@ -11,6 +11,8 @@ from pydantic import BaseModel
 from fastapi import FastAPI
 import openai 
 from dotenv import load_dotenv
+from imblearn.over_sampling import SMOTE
+from sklearn.metrics import classification_report
 
 load_dotenv()
 openai.api_key = os.getenv("API_KEY")
@@ -58,23 +60,31 @@ if not os.path.isfile('mbti_svm.pkl'):
     recreate_model=True
 
 if recreate_model:    
-   # MBTI 데이터셋 불러오기
+    # MBTI 데이터셋 불러오기
     mbti_data = pd.read_csv("MBTI 500.csv")
-
-    # 벡터화
-    tfidf = TfidfVectorizer()
 
     # 데이터셋을 학습용과 테스트용으로 나눔
     X_train, X_test, y_train, y_test = train_test_split(mbti_data['posts'], mbti_data['type'], test_size=0.2, random_state=42)
+
+    # TF-IDF를 사용하여 텍스트를 벡터화
+    tfidf = TfidfVectorizer()
     X_train_tfidf = tfidf.fit_transform(X_train)
+    X_test_tfidf = tfidf.transform(X_test)
 
-    # 서포트 벡터 머신 분류 모델 학습
-    clf = LinearSVC(C=0.4)
-    clf.fit(X_train_tfidf, y_train)
+    # SMOTE를 사용하여 오버샘플링
+    smote = SMOTE(random_state=42)
+    X_train_resampled, y_train_resampled = smote.fit_resample(X_train_tfidf, y_train)
 
-    # 그리드 서치 모델 인스턴스화
-    svm_model = Pipeline([('tfidf',TfidfVectorizer()),('clf',clf)])
-    svm_model.fit(X_train, y_train)
+    # 선형 커널 서포트 벡터 머신
+    svm_model = LinearSVC(C=1.0)
+    svm_model.fit(X_train_resampled, y_train_resampled)
+
+    y_pred = svm_model.predict(X_test_tfidf)
+
+    # 성능 평가
+    report = classification_report(y_test, y_pred)
+    print("Classification Report:")
+    print(report)
 
     # 모델 저장
     joblib.dump(svm_model, 'mbti_svm.pkl')
